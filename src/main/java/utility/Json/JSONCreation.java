d8469731bd8fe018ab01ed23a588766f18d29fe1
proteina <adrianodalessandro@yahoo.it>
2021-01-23 20:16:28 +0100

    Inserita Lib Corretta e JSON

---
+++ src/main/java/utility/Json/JSONCreation.java
@@ -0,0 +1,53 @@
+package utility.Json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+public class JSONCreation {
+
+    private static final String json = null;
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+
+    public static String getJSONToCreate(Object instance) {
+        switch (instance.getClass().getSimpleName()) {
+            case "MovieDb": {
+                try {
+                    return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(JSONCreationMovieDb.getJsonFilmInfo(instance, mapper));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            case "User": {
+                try {
+                    return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(JSONCreationDB.getJsonUser(instance, mapper));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+            case "ArrayList": {
+                ArrayList<Object> list = (ArrayList<Object>) instance;
+                if (list.get(0).getClass().getSimpleName().equals("MovieDb")) {
+                    return JSONCreationMovieDb.getJSONFilmList(instance, mapper);
+                } else {
+                    switch (list.get(0).getClass().getSimpleName()) {
+                        case "UserList": {
+                            //return getJSONUserLists(list,mapper);
+                        }
+                        case "Review": {
+
+                        }
+                        case "User": {
+                            return JSONCreationDB.getJsonListOfUsers(list, mapper);
+                        }
+                    }
+                }
+            }
+        }
+        return json;
+    }
+
+
+}
